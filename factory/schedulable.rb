76ce6f2a2b0f31f4e5724c543ba169151af6aafe
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-09-24 09:22:50 +0900

    update factory pattern

--- factory/schedulable.rb
+++ factory/schedulable.rb
@@ -1,5 +1,5 @@
 class Task
-  attr_accessor :title, :description
+  attr_accessor :title
 
   def initialize(title)
     @title = title
@@ -7,7 +7,7 @@ class Task
 end
 
 class Event
-  attr_accessor :title, :description
+  attr_accessor :title
 
   def initialize(title)
     @title = title
@@ -28,23 +28,41 @@ class Factory
   def create_schedulables
     @schedulables = []
 
-    while input = gets.chomp
-      @schedulables << new_schedulable(input)
+    while title = gets
+      @schedulables << new_schedulable(title)
     end
   end
 
   def output_schedulables
     @schedulables.each do |schedulable|
       puts "タイトル: #{schedulable.title}"
+    end
   end
 end
 
 class TaskFactory < Factory
+  def output_head
+    puts "タスクのタイトルを入力してください"
+  end
+
   def new_schedulable(title)
     Task.new(title)
   end
 end
-  #def new_schedulable(input)
-  #  type, title = input.split
-  #  Object.const_get(type).new(title)
-  #end
+
+class EventFactory < Factory
+  def output_head
+    puts "予定のタイトルを入力してください"
+  end
+
+  def new_schedulable(title)
+    Event.new(title)
+  end
+end
+
+factory = TaskFactory.new
+factory.create_and_output_schedulables
+puts "\n"
+
+factory = EventFactory.new
+factory.create_and_output_schedulables
